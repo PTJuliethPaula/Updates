e9674c89a0711cbf753f6196996a5683584772ae
PaulaAAS0102 <psanchez@passporttechnology.com>
2023-04-14 16:51:08 -0500

    update 04142023

---
+++ BogotaUpdates/20230403/Morongo_04032023_Cs29-17-31-14/FerUpdate/Installer Apr 03 2023/Card Reader Reset/install_Card_Reader_Reset.bat
@@ -0,0 +1,27 @@
+
+REM pushd %PROTOPAS_ROOT%
+
+REM applying rc files
+ccrcconv Registry.rc -A\ -h HKEY_LOCAL_MACHINE -r "SOFTWARE\WOW6432Node\Wincor Nixdorf\ProTopas\CurrentVersion\"
+
+
+REM Property definitions
+call xcopy *.xml C:\ProFlex4\base\CONF\properties\ /y /d /e
+
+
+REM DLL files
+REM call xcopy FEATURES\FEATURE_CUSTOMIZING\PROTOPAS\bin\* bin\ /y /d /e
+
+
+REM jar files
+REM call xcopy *.jar C:\ProFlex4\base\Java\lib\ /y /d /e
+
+
+REM GUIAPP
+REM call xcopy *.html C:\ProFlex4\Base\GUIAPP\content_softkey\views\ /y /d /e
+
+
+REM copying receipt from base to probase
+REM call xcopy *.flc C:\ProFlex4\Base\forms\ptr\*.*  /y /d /e
+
+REM popd
